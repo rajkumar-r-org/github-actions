247e4ad79c2a834d99fa4f6f0b54b2f94ae03f9d
Rajkumar_R <61075470+Rajkumar-21@users.noreply.github.com>
2023-12-30 10:57:07 +0530

    Update pytest-Edge.py

--- .github/scripts/pytest-Edge.py
+++ .github/scripts/pytest-Edge.py
@@ -1,32 +1,24 @@
-import pytest
-import sys
+# importing required package of webdriver
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+# Just Run this to execute the below script
+
+e_serv = Service(EdgeChromiumDriverManager().install())
+e_opts=Options()
+e_opts.add_argument("--headless")
 
 
-@pytest.fixture
-def browser():
-
-    opts = Options()
-    opts.add_argument("headless")
-    driver = webdriver.Edge(options = opts)
-    driver.implicitly_wait(5)
-
-    yield driver
-
-    # For cleanup, quit the driver
-    driver.quit()
-
-
-def test_get_title(browser):
-    browser.get("https://dotnetappgithubactions.azurewebsites.net/")
-
-    assert 'Web App - Unavailable' == browser.title
-
-    elements=browser.page_source
-    print(browser.title)
-
-    assert  'currently stopped' in elements
-    print('Web App is Stopped')
+# Instantiate the webdriver with the executable location of MS Edge web driver
+options = webdriver.EdgeOptions(Service= e_serv, options=e_opts)
+browser = webdriver.Edge(options=options)
+# Simply just open a new Edge browser and go to lambdatest.com
+browser.get('https://www.google.com')
+print(browser.title)
+assert "Google" in browser.title
+# closing the browser
+print(browser.page_source)
+browser.implicitly_wait(10)
+browser.quit()
